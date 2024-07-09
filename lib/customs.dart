adace34f1cf3c5807399789628e8e58165ab1fa8
Taichi USUBA <m21018@g.metro-cit.ac.jp>
2024-07-09 13:50:16 +0900

    定期的なコミット

---
+++ lib/customs.dart
@@ -0,0 +1,13 @@
+// フォント・色・大きさなどをこのファイルで指定して一括管理する
+
+import 'package:flutter/material.dart';
+
+
+// サンプル
+/*
+// BottomNavigationBarの各アイコンの色
+// Color bottomIconButtonColor() {
+//   final bottomIconButtonColor = Color(0xFF000000);
+//   return bottomIconButtonColor;
+// }
+*/
