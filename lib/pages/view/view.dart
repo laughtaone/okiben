2cf2a112f0415255827cfe55b0e325e194252d60
Taichi USUBA <m21018@g.metro-cit.ac.jp>
2024-08-04 13:53:06 +0900

    完了：viewに条件分岐追加

--- lib/pages/view/view.dart
+++ lib/pages/view/view.dart
@@ -75,28 +75,62 @@ class _ViewPageState extends State<ViewPage> {
       // ),
       body: Consumer<OkibenManageModel>(
         builder: (context, model, child) {
-          // 仮に1番目のアイテムが存在する場合に表示する
-          if (model.itemList.length >= 0) {
-            return Center(
-              // child: Text(
-              //   model.itemList[0]['name'],
-              // ),
-              child: ListView.builder(
-                itemCount: model.itemList.length,
-                itemBuilder: (context, index) {
-                  final itemListCopy = model.itemList[index];
-                  return Text(
-                    itemListCopy['name'],
-                    style: TextStyle(fontSize: 30),
-                  );
-                }
-              ),
-            );
-          } else {
-            return Center(
-              child: Text('アイテムがありません'),
-            );
-          }
+          return Padding(
+            padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+            child: Column(
+              children: [
+                CautionWidget(
+                  cautionText: '持ち物がどこにあるか一目でわかります',
+                  backColor: Colors.black12,
+                  textSize: 15.0,
+                  cautionIcon: Icons.check,
+                ),
+                SizedBox(height: 40),
+                Expanded(
+                  child: model.itemList.isEmpty
+                    ? Text(
+                        '登録されている持ち物は0個です',
+                        style: TextStyle(fontSize: 18),
+                      )
+                    : Column(
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              child: Image.asset('assets/images/switch/switch_on.png'),
+                              width: 40,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              '学校にあるもの',
+                              style: TextStyle(
+                                fontSize: 20
+                              ),
+                            )
+                          ]
+                        ),
+                        ListView.builder(
+                          itemCount: model.itemList.length,
+                          itemBuilder: (context, index) {
+                            final itemListCopy = model.itemList[index];
+                            return Text(
+                            itemListCopy['name'],
+                            style: TextStyle(fontSize: 30, color: Colors.black),
+                            );
+                          },
+                          ),
+                      ],
+                    ),
+                ),
+              ],
+            ),
+          );
+          // if (model.itemList.length >= 0) {
+          // } else {
+          //   return Center(
+          //     child: Text('アイテムがありません'),
+          //   );
+          // }
         },
       ),
     );
